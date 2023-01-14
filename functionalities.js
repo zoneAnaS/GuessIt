b5e76813741c45758bbfb9244b5980faca792bc6
Your Name <shaikhanas@gmail.com>
2023-01-15 00:04:09 +0530

    Word checker Funtionality added

--- functionalities.js
+++ functionalities.js
@@ -1,5 +1,13 @@
+let dataOfWords=JSON.parse(sessionStorage.getItem("wordData")) || fetchData();
+var currentWord;
+var worlength;
+//onload function
+onLoad()
 
-getWord()
+
+
+
+
 //function to display button
 function displayButton(){
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
@@ -11,30 +19,72 @@ function displayButton(){
     let btn=document.querySelectorAll(".alphabet-buttons");
     btn.forEach((item)=>{
         item.addEventListener("click",function(){
-            // console.log(this)
-            getWord()
+            let char=item.innerHTML.toLowerCase();
+            for(let i=0;i<currentWord.length;i++){
+                if(char==currentWord[i]){
+                    currentWord.subarr
+                    let wordDiv=document.getElementById(`word${i}`);
+                    wordDiv.classList.remove("wordHidden")
+                    worlength--;
+                }
+                if(worlength<=0){
+                    setTimeout(()=>{
+                        onLoad()
+                    },1000)
+                    
+                }
+            }
         })
     })
     
 }
-displayButton()
 
-
-
-//function to get the word from the database
-async function getWord(){
+//function to get the data from the database
+async function fetchData(){
     let baseUrl="https://63be9d23f5cfc0949b5c0e32.mockapi.io/AllWords/";
-    //all available categories
-    //["Vehicle","Vegetable","Fruit","Land_Animal","Ocean_Animal","Food_Dish","Country","Non_Indian_City","Indian_City","Flower"];
-    //generating random number from 0-9
-    let category_id=Math.ceil(Math.random()*10);
-    let data = await fetch(baseUrl+category_id,{
-
-    })
+    let data = await fetch(baseUrl)
     let res=await data.json();
-    let index=Math.floor(Math.random()*res.Data.length);
-    let catagoryText=document.getElementById("category");
-    let wordText=document.getElementById("word");
-    catagoryText.innerHTML=res.Category.split("_").join(" ");
-    wordText.innerHTML=res.Data[index];
+    // console.log(1)
+    sessionStorage.setItem("wordData",JSON.stringify(res));
+    return res;
+    
 }
+function getWord(){
+    //this funtion returns an array of size 2 where [word,category]
+    let category_id=Math.floor(Math.random()*dataOfWords.length);
+    // console.log(category_id)
+    let index=Math.floor(Math.random()*dataOfWords[category_id].Data.length);
+    return [dataOfWords[category_id].Data[index],dataOfWords[category_id].Category];
+}
+//appends word on the dom
+
+function appendWord(){
+    let [word,cat]=getWord();
+    let wordarr=word.split("");
+    let input_section=document.querySelector(".input-section");
+    let categoryText=document.querySelector("#categoryText");
+    categoryText.innerHTML=cat.split("_").join(" ");
+    input_section.innerHTML="";
+    currentWord=word.toLowerCase();
+    worlength=word.split(" ").join("").length;
+    console.log(currentWord)
+    console.log(worlength)
+    wordarr.forEach((item,i)=>{
+        if(item!=" "){
+            input_section.innerHTML+=`<div class="wordanas">
+            <h1 id=word${i} class="wordHidden">${item.toUpperCase()}</h1>
+        </div>`
+        }else{
+            input_section.innerHTML+=`<div class="spaceLetter">
+            <h1 id=word${i} class="wordHidden"></h1>
+        </div>`
+        }
+        
+    })
+
+}
+function onLoad(){
+    appendWord()
+    displayButton();
+}
+
